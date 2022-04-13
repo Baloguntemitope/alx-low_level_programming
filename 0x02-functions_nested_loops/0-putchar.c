0e3347dfbf31aa09053dbbb818166fa7fdeb89d2
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-04-13 12:16:35 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,11 @@ int main(void)
 {
 	char ermias[] = "_putchar";
 	int c;
-	for(c = 0; c < 8; c++)
+	for (c = 0; c < 8; c++)
 	{
 	_putchar(ermias[c]);
-	}	
+	}
+	
 _putchar('\n');
-	return (0);
+return (0);
 }
