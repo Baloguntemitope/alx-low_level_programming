4bcc345d68a6f698d6dd3f1bd9b0a9de2dd09a60
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-04-13 14:18:40 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,3 @@
-#include <limits.h>
 #include "main.h"
 
 /**
@@ -6,14 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+		_putchar(n + '0');
+		return (n);
 }
