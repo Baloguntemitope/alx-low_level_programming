565238a7d63d7a1f3bf56da5bee5726a23e70d3a
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-04-13 15:15:36 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,10 @@
  */
 int print_last_digit(int n)
 {
-	int ln = n % 10;
+	int n = n % 10;
 
 	if (n < 0)
-		ln = ln * -1;
-	_putchar(ln + '0');
-	return (ln);
+		n = n * -1;
+	_putchar(n + '0');
+	return (n);
 }
