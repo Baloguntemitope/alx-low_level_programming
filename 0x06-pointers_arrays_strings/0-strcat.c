6d018639c928da0b73d03ab11c62e41a61ce5146
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-04-23 21:06:58 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,16 @@
  *
  * Return: a pointer to @dest
  */
-int main(void)
+char *strcat(char *dest, const char *src)
 {
-            char s1[98] = "Hello ";
-                char s2[] = "World!\n";
-                    char *p;
+	int index = 0, dest_len = 0;
 
-                        printf("%s\n", s1);
-                            printf("%s", s2);
-                                p = _strcat(s1, s2);
-                                    printf("%s", s1);
-                                        printf("%s", s2);
-                                            printf("%s", p);
-                                                return (0);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
 
