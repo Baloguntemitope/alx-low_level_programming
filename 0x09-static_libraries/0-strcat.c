b25914b2acdfb4d0a9008c441e3330981235a416
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-05-03 17:22:59 +0100

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,24 @@
-#
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - this fuction will concatenates two strings
+ * @dest: the string that will modified.
+ * @src: the string that ill be concatenated to @des.
+ *
+ * Return: a pointer to @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+	return (dest);
+}
