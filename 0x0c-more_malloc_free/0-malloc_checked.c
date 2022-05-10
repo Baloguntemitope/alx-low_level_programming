5143364800e92f8da66f45ab90ca7ae9d752c5b7
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-05-10 01:54:33 +0100

    Task 0

--- 0x0c-more_malloc_free/0-malloc_checked.c
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,21 @@
-#
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: sizeof int
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *al;
+
+	al = malloc(b);
+	if (al == NULL)
+		exit(98);
+	else
+		return (al);
+}
