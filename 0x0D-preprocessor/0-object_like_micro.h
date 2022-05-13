4fc0505ac1e39427f2fbd8135d2af70b7b24e1d2
Baloguntemitope <101347320+Baloguntemitope@users.noreply.github.com>
2022-05-13 08:20:15 +0100

    Update 0-object_like_micro.h

--- 0x0D-preprocessor/0-object_like_micro.h
+++ 0x0D-preprocessor/0-object_like_micro.h
@@ -1,3 +1,13 @@
-#ifndef SIZE
-#define SIZE (1024)
-#endif /* #indef SIZE */
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth:Balogun yusuf
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
